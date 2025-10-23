9376456984818cb9256f2f051db45c101202e07d
Bilal17809 <khanbangash17809@gmail.com>
2025-10-23 17:37:37 +0500

    by Deployment Tue 22 Oct

--- lib/core/global_keys/global_keys.dart
+++ lib/core/global_keys/global_keys.dart
@@ -1,8 +1,16 @@
 final String examAndSubjectsJson="assets/ems_exam_subjects.json";
 final String subjectQuestionsJson="assets/exam_ems_questions.json";
 
+// android remote keys
 final String androidIntersVal="InterstitialAd";
 final String androidNativeVal="NativeAdvAd";
 final String androidBannerVal="BannerAd";
 final String androidSplashVal="SplashInterstitialAd";
 final String androidOpenAppVal="AppOpenAd";
+
+// ios remote keys
+final String iosIntersVal="InterstitialAd";
+final String iosNativeVal="NativeAdvAd";
+final String iosBannerVal="BannerAd";
+final String iosSplashVal="SplashInterstitialAd";
+final String iosOpenAppVal="AppOpenAd";
